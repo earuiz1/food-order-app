0a74035a7985d6c5da707edf7a0d197e39121041
earuiz1 <earuiz1196@gmail.com>
2023-02-07 16:00:09 -0600

    Added a section with a loading spinner

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Cart from "./components/Cart/Cart";
 import { CartProvider } from "./context/cartContext";
 import { db } from "./firebase";
 import { collection, getDocs } from "@firebase/firestore";
+import { ImSpinner2 } from "react-icons/im";
 
 const App = () => {
   const [meals, setMeals] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  /* Getting the data from the firebase database and setting it to the state. */
   useEffect(() => {
     const getData = async () => {
       const mealsData = await getDocs(collection(db, "meals"));
       setMeals(mealsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       console.log(meals);
+      setIsLoading(false);
     };
 
     getData();
@@ -34,7 +36,14 @@ const App = () => {
       {isModalOpen && <Cart onClose={closeModal} />}
       <Nav onOpen={openModal} />
       <HeaderMain />
-      <MenuList meals={meals} />
+      {isLoading ? (
+        <section className="flex justify-center w-full my-20 gap-3">
+          <ImSpinner2 className="text-slate-50 animate-spin" size={30} />
+          <span className="text-slate-50 font-bold">Loading...</span>
+        </section>
+      ) : (
+        <MenuList meals={meals} />
+      )}
     </CartProvider>
   );
 };
